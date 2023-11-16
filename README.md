8f689113b69a40f49489e051afd0d5a574fceaba
Pacotacou <pacomoises01@gmail.com>
2023-11-16 05:13:14 -0500

    ready 1.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tambaleo-del-borracho-electron
  
+para instalar:
+npm install
+
+para correr:
+npm test
--- src/menu_principal.js
+++ src/menu_principal.js
@@ -1,12 +1,39 @@
 window.$ = window.jQuery = require('jquery');
 const {monteCarloSimulation} = require('./class/DrunkWalk.js')
+const {Chart} = require('chart.js/auto');
 
 document.addEventListener('DOMContentLoaded',()=>{
-    btnSimular = document.getElementById('btnSimular');
-    tableResultados = document.getElementById('tableResultados');
-    spanPromedio = document.getElementById('span-promedio');
-    inputM = document.getElementById('input-m');
-    inputN = document.getElementById('input-n');
+    let btnSimular = document.getElementById('btnSimular');
+    let tableResultados = document.getElementById('tablaResultados');
+    let spanPromedio = document.getElementById('span-promedio');
+    let inputM = document.getElementById('input-m');
+    let inputN = document.getElementById('input-n');
+
+    let grafica
+
+    const crearGrafica = (datos)=>{
+        if(grafica ){
+            grafica.destroy();
+        }
+        grafica = new Chart(document.getElementById("grafica"),{
+            type: 'line',
+            data: {
+                labels: datos.map((i)=>{
+                    return i.x.toFixed(2);
+                }),
+                datasets: [{
+                    label:'Tambaleo del borracho',
+                    data: datos.map((i)=>{
+                        return i.y.toFixed(2);
+                    }),
+                    fill:false
+                }]
+                
+            }
+        })
+
+        
+    }
 
     btnSimular.addEventListener('click',()=>{
         try {
@@ -22,15 +49,15 @@ document.addEventListener('DOMContentLoaded',()=>{
                 tr.innerHTML = 
                 `
                 <td>${row.paso}</td>
-                <td>${row.x}</td>
-                <td>${row.y}</td>
-                <td>${row.distancia}</td>
+                <td>${row.x.toFixed(4)}</td>
+                <td>${row.y.toFixed(4)}</td>
+                <td>${row.distancia.toFixed(4)}</td>
                 `
                 tbody.appendChild(tr);
             }
             tableResultados.innerHTML = tbody.innerHTML;
-            spanPromedio.textContent = promedio
-
+            spanPromedio.textContent = promedio;
+            crearGrafica(datos);
         } catch (error) {
             new Notification(error);
         }
